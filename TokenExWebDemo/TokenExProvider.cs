04a1fc768a33600990ec23570bd01b1665762e8e
Mason Harper <mharper@tokenex.com>
2020-01-23 14:56:07 -0600

    Updated iframe example to v3

--- TokenExWebDemo/TokenExProvider.cs
+++ TokenExWebDemo/TokenExProvider.cs
@@ -1,37 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Net.Http;
-
 
 namespace TokenExWebDemo
 {
     public class TokenExAPIProvider
     {
-
         private HttpResponseMessage MakeAPICall<T>(object dto, string url)
         {
-            HttpResponseMessage result = new HttpResponseMessage();
+            HttpResponseMessage result;
 
-            try
+            using (var client = new HttpClient())
             {
-                using (HttpClient client = new HttpClient())
-                {
-
-                    result = client.PostAsJsonAsync<T>(url, (T)dto).Result;
-
-                }
-                if (!result.IsSuccessStatusCode)
-                {
-                    throw new ApplicationException($"oops {result.StatusCode.ToString()}");
-
-                }
+                result = client.PostAsJsonAsync<T>(url, (T)dto).Result;
             }
-            catch (Exception)
+            if (!result.IsSuccessStatusCode)
             {
-
-                throw;
+                throw new ApplicationException($"Oops! Encountered error: {result.StatusCode.ToString()}");
             }
 
             return result;
--- TokenExWebDemo/Utility.cs
+++ TokenExWebDemo/Utility.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text;
+using TokenExWebDemo.Models;
 
 namespace TokenExWebDemo
 {
@@ -19,6 +20,42 @@ namespace TokenExWebDemo
             var years = new Dictionary<string, string>();
             Enumerable.Range(DateTime.Now.Year, 10).ToList().ForEach(x => years.Add(x.ToString(), x.ToString()));
             return years;
+        }
+
+        /// <summary>
+        /// Using a given origin, generate an instance of an IframeConfiguration
+        /// </summary>
+        /// <param name="origin">The page from which the request originated</param>
+        public static IframeConfiguration GenerateIframeConfiguration(string origin)
+        {
+            // Generate Authentication Key
+            var timestamp = DateTime.UtcNow.ToString("yyyyMMddHHmmss");
+            var concatenatedString = $"{Config.TokenExID}|{origin}|{timestamp}|{Config.TokenScheme}";
+            var authenticationKey = GenerateHMAC(concatenatedString, Config.ClientSecretKey);
+
+
+            return new IframeConfiguration()
+            {
+                Origin = origin,
+                Timestamp = timestamp,
+                TokenExID = Config.TokenExID,
+                AuthenticationKey = authenticationKey,
+                TokenScheme = Config.TokenScheme
+            };
+        }
+
+        /// <summary>
+        /// Valid HMAC signature to detect modifications from the browser
+        /// </summary>
+        /// <param name="payload">The information to hash</param>
+        /// <param name="HMACKey">The SHA256 key to use in hashing</param>
+        public static string GenerateHMAC(string payload, string HMACKey)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA256(Encoding.UTF8.GetBytes(HMACKey)))
+            {
+                var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(payload));
+                return Convert.ToBase64String(hash);
+            }
         }
     }
 }
